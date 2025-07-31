87bc0bde99da4a2c1c288a70cf3a324c70a1e7f1
lmo0317 <lmo0317@naver.com>
2025-07-31 15:19:32 +0900

    매크로 수정

--- lastwawr_gold_zombie.sikuli/lastwawr_gold_zombie.py
+++ lastwawr_gold_zombie.sikuli/lastwawr_gold_zombie.py
@@ -2,11 +2,9 @@ IMG_SPECIAL_EVENT = "1753892268982.png" # 특별 이벤트
 IMG_SEARCH_BUTTON = "1753892300669.png" # 검색
 IMG_SWORD_ICON   = "1753892320825.png" # 칼그림
 IMG_DEPLOY       = "1753892339850.png" # 출정
-
 IMG_RECOVER      = "1753892371144.png" #체력 회복
 IMG_USE          = "1753892388978.png" # 사용
 IMG_CLOSE         = "1753892408517.png" # X 버튼
-
 
 #use 버튼 클릭
 def click_use_button():
@@ -41,19 +39,19 @@ def recover_if_needed():
         print(">> 체력 회복 버튼 없음")
 
 while True:
-    
-    click(Pattern(IMG_SPECIAL_EVENT).similar(0.5)) #특별 이벤트
-    wait(1)
-
-    click(Pattern(IMG_SEARCH_BUTTON).similar(0.5)) #검색
-    wait(1)
-    
-    click(Pattern(IMG_SWORD_ICON).similar(0.5)) #칼 그림
-    wait(1)
-
-    recover_if_needed()  # 함수 호출     
-    
-    click(Pattern(IMG_DEPLOY).similar(0.5)) #출정
-    wait(1)
-    
-    wait(60)
+    try:
+        click(Pattern(IMG_SPECIAL_EVENT).similar(0.5))  # 특별 이벤트
+        wait(1)
+        click(Pattern(IMG_SEARCH_BUTTON).similar(0.5))  # 검색
+        wait(1)
+        click(Pattern(IMG_SWORD_ICON).similar(0.5))     # 칼 그림
+        wait(1)
+        recover_if_needed()                             # 체력 회복
+        click(Pattern(IMG_DEPLOY).similar(0.5))         # 출정
+        wait(1)
+        wait(30)  # 다음 루프 대기
+    except FindFailed:
+        print(">> 루프 중 예외 발생. ESC 키 누름 후 계속 진행")
+        type(Key.ESC)
+        wait(1)
+        continue
